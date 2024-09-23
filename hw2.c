d24610c9e41dcd46cd52e4160d0fbf713fac58f0
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2024-09-22 23:07:06 -0400

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -11,8 +11,6 @@ int main(int argc, char *argv[]) {
     ssize_t bytes_read, bytes_written;
     char buffer[BUFFER_SIZE];
     int user_id;
-    char uid_input[20]; // Buffer to store user ID input
-    ssize_t uid_bytes_read;
 
     // Open the input file for reading
     input_fd = open(argv[1], O_RDONLY);
@@ -21,37 +19,22 @@ int main(int argc, char *argv[]) {
     // Hardcoded full name
     char full_name[] = "Daniel Zhang";
 
-    // Prompt the user to enter their user ID
-    const char prompt_msg[] = "Please enter your user ID: ";
-    write(STDOUT_FILENO, prompt_msg, strlen(prompt_msg));
-
-    // Read the user ID from terminal input using system calls
-    uid_bytes_read = read(STDIN_FILENO, uid_input, sizeof(uid_input) - 1);
-    uid_input[uid_bytes_read] = '\0'; // Null-terminate the string
-
-
-    // Convert user ID string to integer using strtol()
-    char *endptr;
-    user_id = strtol(uid_input, &endptr, 10);
-
+    // Get the user ID using the system call `getuid()`
+    user_id = getuid();
 
     // Write the full name to the output file
     bytes_written = write(output_fd, full_name, strlen(full_name));
 
-
     // Write a space separator
     bytes_written = write(output_fd, " ", 1);
-
 
     // Write the user ID to the output file
     char uid_str[20];
     snprintf(uid_str, sizeof(uid_str), "%d", user_id);
     bytes_written = write(output_fd, uid_str, strlen(uid_str));
 
-
     // Write a newline character
     bytes_written = write(output_fd, "\n", 1);
-
 
     // Copy the contents from the input file to the output file
     while ((bytes_read = read(input_fd, buffer, BUFFER_SIZE)) > 0) {
@@ -61,8 +44,6 @@ int main(int argc, char *argv[]) {
             total_written += bytes_written;
         }
     }
-
-
 
     // Close the file descriptors
     close(input_fd);
