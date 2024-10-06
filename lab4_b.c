c7a5b2e7e5cc0deedf6a4e2fdba65e856b0c6b0b
dpz1707 <90921421+dpz1707@users.noreply.github.com>
2024-10-06 19:09:38 -0400

    Create lab4_b.c

---
+++ lab4_b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    // parent
+    if (fork() == 0) { 
+        // child 1
+        if (fork() == 0) {
+            // child 3 
+            exit(0);
+        }
+        exit(0);
+    } else {
+        if (fork() == 0) {
+            // child 2
+            if (fork() == 0) {
+                // child 4
+                exit(0);
+            }
+            exit(0);
+        }
+    }
+    sleep(1);  // To prevent conflicts
+    return 0;
+}
